1b2bf55641cf113a72417b787f47c85929b2549d
jon <can158@gmail.com>
2021-06-17 18:15:14 -0700

    updates to the rotating squares. looks dope

--- rotating_squares_processing.py
+++ rotating_squares_processing.py
@@ -1,6 +1,7 @@
 from random import randint
 from math import *
 import time
+import traceback
 
 # Config
 WINDOW_WIDTH = 64 * 10
@@ -18,7 +19,7 @@ V_MAX = 100
 
 tau = 2 * pi
 
-DEFAULT_FILL_COLOR = (0, 80, 100)
+DEFAULT_FILL_COLOR = (160, 30, 100)
 DEFAULT_STROKE_COLOR = (0, 0, 0)
 DEFAULT_BACKGROUND_COLOR = (0, 0, 0) # WHITE
 
@@ -47,15 +48,17 @@ class Oscillator:
 
 # Square that is centered at x, y, with each edge being edgeLen length.
 class CustomSquare:
-    def __init__(self, x, y, edgeLen, thickness=1, angle=0):
+    def __init__(self, x, y, edgeLen, thickness=1, angle=0, edgeLenUpSpeed=999, edgeLenDownSpeed=999):
         self.x = x
         self.y = y
-        self.edgeLen = edgeLen
+        self.edgeLen = self.edgeLenTarget = edgeLen
         self.thickness = thickness
         self.angle = angle
         self.calculateCoords()
         self.fillColor = DEFAULT_FILL_COLOR
         self.strokeColor = DEFAULT_STROKE_COLOR
+        self.edgeLenUpSpeed = edgeLenUpSpeed
+        self.edgeLenDownSpeed = edgeLenDownSpeed
 
     def setFillColor(self, hsv):
         self.fillColor = hsv
@@ -63,9 +66,15 @@ class CustomSquare:
     def setStrokeColor(self, hsv):
         self.strokeColor = hsv
 
+    def setSaturation(self, s):
+        self.strokeColor = (self.strokeColor[0], s, self.strokeColor[2])
+
     def setEdgeLen(self, edgeLen):
-        self.edgeLen = edgeLen
+        self.edgeLen = self.setEdgeLenTarget = edgeLen
         self.calculateCoords()
+
+    def setEdgeLenTarget(self, edgeLenTarget):
+        self.edgeLenTarget = edgeLenTarget
 
     def setThickness(self, thickness):
         self.thickness = thickness
@@ -109,6 +118,24 @@ class CustomSquare:
             self.edgeLen = 0
         self.calculateCoords()
 
+    def update(self):
+        global DEFAULT_FILL_COLOR
+        if (self.edgeLen != self.edgeLenTarget):
+            direction = (-1 if self.edgeLenTarget < self.edgeLen else 1)
+            if (direction == 1):
+                self.edgeLen += self.edgeLenUpSpeed
+            else:
+                self.edgeLen -= self.edgeLenDownSpeed
+            if ((self.edgeLen > self.edgeLenTarget and direction == 1) or \
+                (self.edgeLen < self.edgeLenTarget and direction == -1)):
+                self.edgeLen = self.edgeLenTarget
+            self.calculateCoords()
+        # coeff = self.edgeLen/100 #d/WINDOW_WIDTH
+        coeff = self.edgeLen * 30
+        self.rotate((coeff if coeff <= 360 else 360))
+        c = ((DEFAULT_FILL_COLOR[0])%H_MAX, (DEFAULT_FILL_COLOR[1]+(coeff/10)), DEFAULT_FILL_COLOR[2])
+        self.setStrokeColor(c)
+
     def draw(self):
         # fill(self.fillColor[0], self.fillColor[1], self.fillColor[2])
         noFill()
@@ -116,7 +143,8 @@ class CustomSquare:
         strokeWeight(self.thickness)
         quad(self.x1, self.y1, self.x2, self.y2, self.x3, self.y3, self.x4, self.y4)
         if (DEBUGVISUALS):
-            circle(self.x, self.y, 1)
+            circleWidth = 1 #(1 if self.edgeLen/10 < 1 else self.edgeLen)
+            circle(self.x, self.y, circleWidth)
 
 class Canvas:
     def __init__(self):
@@ -124,34 +152,32 @@ class Canvas:
         self.s = 100
         self.initx = WINDOW_WIDTH*5/6
 
-        # lets have a 4 by 4
-        # lets have a larger area for each square
-        self.s = CustomSquare(10, 10, 10, 1)
-
+        # SQUARES
         self.squares = []
         self.numSquaresPerRow = 30
         self.squareEdgeLen = 10
         self.squareAreaEdgeLen = 20
-        self.xOffset = self.yOffset = 10
+        self.xOffset = self.yOffset = 30
         for i in range(0, self.numSquaresPerRow):
             for j in range(0, self.numSquaresPerRow):
-                self.squares.append(CustomSquare(self.xOffset + (i * self.squareAreaEdgeLen), self.yOffset + (j * self.squareAreaEdgeLen), self.squareEdgeLen, 1))
+                self.squares.append(CustomSquare(self.xOffset + (i * self.squareAreaEdgeLen), self.yOffset + (j * self.squareAreaEdgeLen), self.squareEdgeLen, 2, edgeLenUpSpeed=1, edgeLenDownSpeed=0.1))
 
+        # OSCILLATORS
         self.oscillators = []
+        self.oscillators.append(Oscillator(0.01))
 
     def reset(self):
         background(*DEFAULT_BACKGROUND_COLOR)
 
     def update(self):
+        global DEFAULT_FILL_COLOR
         for o in self.oscillators:
             o.update()
         
         for sidx, s in enumerate(self.squares):
-            # s.replace(self.initx - (WINDOW_WIDTH*2/6) - (WINDOW_WIDTH*2/6 * self.sizeOsc.getVal()), s.y)
-            # s.resize(200 * self.sizeOsc.getVal())
-            s.setStrokeColor(DEFAULT_FILL_COLOR)
-            # s.rotate((sidx*4) + 360 * self.rotOsc.getVal())
-            pass
+            s.update()
+
+        # DEFAULT_FILL_COLOR = ((DEFAULT_FILL_COLOR[0]+1)%H_MAX, DEFAULT_FILL_COLOR[1], DEFAULT_FILL_COLOR[2])
 
     def drawCanvas(self):
         background(*DEFAULT_BACKGROUND_COLOR)
@@ -161,10 +187,10 @@ class Canvas:
     def mouseInput(self, x, y):
         for s in self.squares:
             d = dist(x, y, s.x, s.y)
-            edgeLen = sqrt(d)
+            edgeLen = 100/sqrt(d)
             # thickness = d/100
-            # edgeLen = (50 if edgeLen > 50 else edgeLen)
-            s.setEdgeLen(edgeLen)
+            edgeLen = (self.squareAreaEdgeLen if edgeLen > self.squareAreaEdgeLen else edgeLen)
+            s.setEdgeLenTarget(edgeLen)
             # s.setThickness(thickness)
     
     def rotateAll(self, rotate):
@@ -182,8 +208,6 @@ def togglePause():
     global playing
     playing = ~playing
     DEBUGPRINT("Playing" if playing else "Paused")
-
-
 
 ###########################################
 
