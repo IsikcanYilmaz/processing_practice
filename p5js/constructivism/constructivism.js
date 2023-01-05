60f1d9c044ee3ebd938473396f07a0f5f9665265
jon <can158@gmail.com>
2023-01-05 20:48:49 +0300

    constructivism begun. templates

---
+++ p5js/constructivism/constructivism.js
@@ -0,0 +1,106 @@
+var WINDOW_HEIGHT = 800;
+var WINDOW_WIDTH  = 800;
+
+var H_MAX = 360;
+var S_MAX = 100;
+var V_MAX = 100;
+
+var DEFAULT_BACKGROUND = [0, 0, 0];
+var DEFAULT_STROKE_COLOR = [250, 0, 100];
+
+var DEBUGVISUALS = false;
+
+var PI  = Math.PI;
+var TAU = Math.PI * 2;
+
+////////////////////////
+
+var SAVE_FRAMES = false;
+
+////////////////////////
+
+class Canvas 
+{
+  constructor()
+  {
+  }
+
+  updateCanvas()
+  {
+  }
+
+  drawCanvas()
+  {
+  }
+
+  drawDebugPanel()
+  {
+  }
+
+  saveFrame()
+  {
+  }
+}
+
+////////////////////////
+// I/O
+
+// Mouse
+function mouseClickedGeneric(arr)
+{
+  var [x, y] = arr;
+  myCanvas.mouseInput(x, y);
+}
+
+function mouseDragged()
+{
+  mouseClickedGeneric([mouseX, mouseY]);
+}
+
+function mouseMoved()
+{
+}
+
+function mouseWheel()
+{
+}
+
+// Keyboard
+function keyPressedGeneric(arr)
+{
+  var [k, x, y] = arr;
+  var mx = x || 0;
+  var my = y || 0;
+  console.log("KEY PRESSED", k, "WITH", x, y, arr);
+}
+
+function keyPressed()
+{
+  keyPressedGeneric([key, mouseX, mouseY]);
+}
+
+function keyReleased()
+{
+  console.log("KEY RELEASED", key);
+}
+
+
+////////////////////////
+
+myCanvas = new Canvas();
+p5jsCanvas = undefined;
+function setup()
+{
+  p5jsCanvas = createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+  colorMode(HSB, H_MAX, S_MAX, V_MAX);
+  background(DEFAULT_BACKGROUND);
+  textSize(12);
+  smooth(8);
+}
+
+function draw()
+{
+  myCanvas.updateCanvas();
+  myCanvas.drawCanvas();
+  myCanvas.drawDebugPanel();
+}
