691364457aa6a356a43876bf0296907c88cd00cd
jon <isikcan.yilmaz@tu-dresden.de>
2025-03-20 17:07:12 +0100

    rng_snack

---
+++ p5js/rng_snack_art/rng_snack_art.js
@@ -0,0 +1,190 @@
+var WINDOW_HEIGHT = 800;
+var WINDOW_WIDTH  = 800;
+
+var H_MAX = 360;
+var S_MAX = 100;
+var V_MAX = 100;
+
+var DEFAULT_BACKGROUND = [0, 0, 100];
+var DEFAULT_STROKE_COLOR = [250, 0, 100];
+
+var DEBUGVISUALS = false;
+
+var PI  = Math.PI;
+var TAU = Math.PI * 2;
+
+////////////////////////
+
+var SAVE_FRAMES = false;
+
+var FRAME_LIMITING = false;
+var FRAME_PER_SECOND = 60;
+var SAVE_NUM_FRAMES = FRAME_PER_SECOND * 50;
+if (SAVE_FRAMES)
+{
+  FRAME_LIMITING = true;
+  FRAME_PER_SECOND = 15;
+}
+var FRAME_PERIOD_MS = 1000 / FRAME_PER_SECOND;
+
+var TOGGLE_DEBUG_ALLOWED = false;
+var DEBUG_LINES = false;
+var DEBUG_FPS = false;
+
+var GRID_LEN = 16;
+var CELL_LEN_PX = (WINDOW_WIDTH / GRID_LEN);
+
+class Grid
+{
+  constructor(len)
+  {
+    this.len = len;
+  }
+
+  draw()
+  {
+    stroke(0, 100, 0);
+    noFill();
+    strokeWeight(1);
+
+    for (var i = 0; i < GRID_LEN; i++)
+    {
+      for (var j = 0; j < GRID_LEN; j++)
+      {
+        rect(i * CELL_LEN_PX, j * CELL_LEN_PX, CELL_LEN_PX, CELL_LEN_PX);
+      }
+    }
+
+  }
+}
+
+////////////////////////
+
+class Canvas 
+{
+  constructor()
+  {
+		this.config = {
+									};
+    this.grid = new Grid(GRID_LEN);
+  }
+
+  updateCanvas(currFrame)
+  {
+  }
+
+  drawCanvas(currFrame)
+  {
+    background(DEFAULT_BACKGROUND);
+    this.grid.draw();
+  }
+  
+
+  drawDebugPanel()
+  {
+  }
+
+  saveFrame()
+  {
+		var filename = "rng_snack-" + str(currFrame).padStart(5, "0");
+		saveCanvas(p5jsCanvas, filename, "svg");
+		console.log("SAVED FRAME", this.frameId);
+	}
+}
+
+////////////////////////
+
+function mouseMoved()
+{
+}
+
+function mouseWheel()
+{
+}
+
+function keyPressed()
+{
+  console.log("KEY PRESSED", key);
+  if (key == ' ')
+  {
+    myCanvas.paused = false;
+  }
+}
+
+function keyReleased()
+{
+  console.log("KEY RELEASED", key);
+  if (key == ' ')
+  {
+    myCanvas.paused = true;
+  }
+  if (key == 'd' && TOGGLE_DEBUG_ALLOWED)
+  {
+    background(0, 0, 0);
+    DEBUG_LINES = !DEBUG_LINES;
+    DEBUG_FPS = !DEBUG_FPS;
+  }
+}
+
+
+////////////////////////
+
+myCanvas = new Canvas();
+p5jsCanvas = undefined;
+function setup()
+{
+  p5jsCanvas = createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+  colorMode(HSB, H_MAX, S_MAX, V_MAX);
+  background(DEFAULT_BACKGROUND);
+  textSize(12);
+  smooth(8);
+}
+
+var lastFrameTs = 0;
+var fps = 0;
+var timeSinceLastFrameMsMs = 0;
+
+var gui = new dat.GUI({hideable:true});
+
+var folder1 = gui.addFolder("Knobs");
+//folder1.add(myCanvas.config, "h", -H_MAX, H_MAX);
+//folder1.add(myCanvas.config, "s", -S_MAX, S_MAX);
+//folder1.add(myCanvas.config, "v", -V_MAX, V_MAX);
+//folder1.add(myCanvas.config, "l", 0, 50);
+//folder1.add(myCanvas.config, "hChange", -H_MAX, H_MAX);
+//folder1.add(myCanvas.config, "sChange", -10, 10);
+//folder1.add(myCanvas.config, "vChange", -10, 10);
+//folder1.add(myCanvas.config, "lChange", 1, 100);
+//folder1.add(myCanvas.config, "strokeWeight", 0, 30);
+
+var folder2 = gui.addFolder("Oscillators");
+//folder2.add(myCanvas.config, "oscillators", false, true);
+//folder2.add(myCanvas.config, "osc1Freq", 0.01, 2);
+//folder2.add(myCanvas.config, "osc2Freq", 0.01, 2);
+//folder2.add(myCanvas.config, "osc3Freq", 0.01, 2);
+//folder2.add(myCanvas.config, "hueOscFreq", 0.001, 2);
+
+folder1.open();
+folder2.open();
+
+var currFrame = 0;
+
+function draw()
+{
+  var frameTs = Date.now();
+  if (lastFrameTs != 0)
+  {
+    timeSinceLastFrameMsMs = frameTs - lastFrameTs;
+    fps = int(1000 / timeSinceLastFrameMsMs);
+
+    if (FRAME_LIMITING && timeSinceLastFrameMsMs < FRAME_PERIOD_MS)
+    {
+      return;
+    }
+  }
+  lastFrameTs = frameTs;
+  myCanvas.updateCanvas(currFrame);
+  myCanvas.drawCanvas(currFrame);
+  myCanvas.drawDebugPanel();
+	currFrame++;
+}
